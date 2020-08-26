59bb944f83add261c0274286cb4c9e9f7ed202d1
Jakub <jakub.raban@gmail.com>
2020-08-26 17:11:57 +0200

    Added expired lock actions: all available lock operations accessible from app

--- src/main/js/components/ExpiredLockActions.js
+++ src/main/js/components/ExpiredLockActions.js
@@ -1,33 +1,42 @@
 import React from 'react'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import Button from 'react-bootstrap/Button'
-import LockCreateConfirmationModal from "./LockCreateConfirmationModal";
-import LockDeleteWhenActiveModal from "./LockDeleteWhenActiveModal";
-import LockActivateModal from "./LockActivateModal";
 import {connect} from 'react-redux'
-import {openLock} from "../redux/actions";
+import {openLock, activateLock, deleteLock} from "../redux/actions";
+import LockOpenedModal from "./LockOpenedModal";
+import LockActivateModal from "./LockActivateModal";
+import LockDeleteWhenActiveModal from "./LockDeleteWhenActiveModal";
+import {getLockStatus, OPENED} from "../LockStatus";
 
 class ExpiredLockActions extends React.Component {
 
     getPasswordBackRef = React.createRef()
+    reactivateRef = React.createRef()
+    deleteRef = React.createRef()
 
     getPasswordBack = () => {
         if (!this.props.lock.plainTextPassword) {
             this.props.openLock(this.props.lock)
         }
-        this.getPasswordBackRef.current.showWithData(this.props.lock.name, this.props.lock.plainTextPassword)
+        this.getPasswordBackRef.current.show()
     }
+
+    onReactivateClick = () => this.reactivateRef.current.show()
+    reactivateLock = timeInSeconds => this.props.activateLock(this.props.lock, timeInSeconds)
+
+    onDeleteClick = () => getLockStatus(this.props.lock) === OPENED ? this.deleteLock() : this.deleteRef.current.show()
+    deleteLock = () => this.props.deleteLock(this.props.lock)
 
     render() {
         return (
             <>
-                <LockCreateConfirmationModal ref={this.getPasswordBackRef} isOpening={true}/>
-                {/*<LockActivateModal />*/}
-                {/*<LockDeleteWhenActiveModal />*/}
+                <LockOpenedModal ref={this.getPasswordBackRef} lock={this.props.lock}/>
+                <LockActivateModal ref={this.reactivateRef} lock={this.props.lock} isFirstActivation={false} activateCallback={this.reactivateLock}/>
+                <LockDeleteWhenActiveModal ref={this.deleteRef} lock={this.props.lock} deleteCallback={this.deleteLock}/>
                 <ButtonGroup size={"sm"}>
                     <Button variant={"success"} onClick={this.getPasswordBack}>Get your password back</Button>
-                    <Button variant={"primary"}>Activate again</Button>
-                    <Button variant={"danger"}>Delete</Button>
+                    <Button variant={"primary"} onClick={this.onReactivateClick}>Activate again</Button>
+                    <Button variant={"danger"} onClick={this.onDeleteClick}>Delete</Button>
                 </ButtonGroup>
             </>
         );
@@ -35,4 +44,4 @@ class ExpiredLockActions extends React.Component {
 
 }
 
-export default connect(null, {openLock})(ExpiredLockActions);
+export default connect(null, {openLock, activateLock, deleteLock})(ExpiredLockActions);
